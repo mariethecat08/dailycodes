a5619f62f9e4cf21ce5f69e850b0bc632b2f7766
aaaada13 <jiy081332@gmail.com>
2023-03-30 21:07:15 +0900

    10989_v2

--- 10989.py
+++ 10989.py
@@ -1,5 +1,13 @@
+from collections import defaultdict
 n = int(input())
-nums = []
-for i in range(n):
-    nums.append(int(input()))
-print(sorted(nums))
+numbers = defaultdict(lambda : 0)
+
+for _ in range(n):
+    num = input()
+    numbers[num] += 1
+
+sort_nums = sorted(numbers.keys())
+
+for n in sort_nums:
+    for cnt in range(int(numbers[n])):
+        print(n)
