fe0764ec7999896a3f92586a156f9c3eb1be4e29
aaaada13 <jiy081332@gmail.com>
2023-04-03 10:48:20 +0900

    3/30

--- 10989.py
+++ 10989.py
@@ -1,13 +1,16 @@
-from collections import defaultdict
-n = int(input())
-numbers = defaultdict(lambda : 0)
+# from collections import defaultdict
+# n = int(input())
+# numbers = defaultdict(lambda : 0)
 
-for _ in range(n):
-    num = input()
-    numbers[num] += 1
+# for _ in range(n):
+#     num = input()
+#     numbers[num] += 1
 
-sort_nums = sorted(numbers.keys())
+# sort_nums = sorted(numbers.keys())
 
-for n in sort_nums:
-    for cnt in range(int(numbers[n])):
-        print(n)
+# for n in sort_nums:
+#     print((n+"\n")*numbers[n])
+    # for cnt in range(int(numbers[n])):
+    #     print(n)
+print("o"*2)
+print("----------")
